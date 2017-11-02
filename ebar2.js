b2db6b605c84e8f544d345b44d648fbfcacf6ff9
Bernarddebruin <33249285+Bernarddebruin@users.noreply.github.com>
2017-11-02 08:14:56 +0100

    Markline fix/ definition split

--- ebar2.js
+++ ebar2.js
@@ -1,215 +1,9 @@
 //https://ecomfe.github.io/echarts-examples/public/editor.html?c=bar1 <-- Source file from echarts
-define(["./echarts-en", "qlik"],function(echarts, qlik){
+define(["./echarts-en", "qlik", "./definition"],function(echarts, qlik, definition){
   var app = qlik.currApp();
   console.log(qlik);
   return{
-    definition: {
-      component: "accordion",
-      type: "items",
-      items: {
-        dimensions: {
-          uses: "dimensions",
-          min: 1,
-          max: 2,
-        },
-        measures: {
-          uses: "measures",
-          min: 2,
-          max: 2
-        },
-        settings: {
-          uses: "settings"
-        },
-        layout: {
-          label: 'Layout',
-          component: "expandable-items",
-          items: {
-            colorPicking:{
-              label: "Color Picker",
-              items: {
-                colorMeasure1: {
-                  ref: "meas1.Colour",
-                  label: "Colour Measure 1",
-                  component: "color-picker",
-                  dualOutput: true,
-                  type: "object",
-                  defaultValue: {
-                    color: "#CCCCCC",
-                    index: -1
-                  }
-                },
-                colorMeasure2: {
-                  ref: "meas2.Colour",
-                  label: "Colour Measure 2",
-                  component: "color-picker",
-                  dualOutput: true,
-                  type: "object",
-                  defaultValue: {
-                    color: "#CCCCCC",
-                    index: -1
-                  }
-                }
-              }
-            },
-            toolbox: {
-              label: "Toolbox",
-              items: {
-                toolboxHandle: {
-                  type: 'string',
-                  component: 'dropdown',
-                  label: 'Toolbox Visable/Hidden',
-                  ref: 'toolboxHandle',
-                  deafault: true,
-                  options: [{
-                    value: true,
-                    label: 'Visable'
-                  },{
-                    value: false,
-                    label: 'Hidden'
-                  }]
-                },
-                lineChart: {
-                  type: "boolean",
-                  label: "Line Chart",
-                  ref: "toolboxLine",
-                  defaultValue: false
-                },
-                barChart: {
-                  type: "boolean",
-                  label: "Bar chart",
-                  ref: "toolboxBar",
-                  defaultValue: false
-                },
-                stacked: {
-                  type: "boolean",
-                  label: "Stacked charts",
-                  ref: "toolboxStacked",
-                  defaultValue: false
-                },
-                toolboxRestore: {
-                  type: 'string',
-                  component: 'dropdown',
-                  label: 'Toolbox Restore on/off',
-                  ref: 'toolboxRestore',
-                  options: [{
-                    value: true,
-                    label: 'On'
-                  },{
-                    value: false,
-                    label: 'Off'
-                  }]
-                },
-                toolboxDataview: {
-                  type: 'string',
-                  component: 'dropdown',
-                  label: 'Toolbox show Data on/off',
-                  ref: 'toolboxDataview',
-                  options: [{
-                    value: true,
-                    label: 'On'
-                  },{
-                    value: false,
-                    label: 'Off'
-                  }]
-                },
-              }
-            },
-            marktetOptions:{
-              label: "Marktpoint Options",
-              component: 'expandable-items',
-              items: {
-                marktLine: {
-                  type: 'string',
-                  component: 'dropdown',
-                  label: 'Show Markt Line on/off',
-                  ref: 'showMarktLine',
-                  default: true,
-                  options: [{
-                    value: true,
-                    label: 'On'
-                  },{
-                    value: false,
-                    label: 'Off'
-                  }]
-                },
-                marktPoint: {
-                  label: 'Marktpoint',
-                  items: {
-                      marktLine: {
-                        type: 'string',
-                        component: 'dropdown',
-                        label: 'Show Markt Pointer on/off',
-                        ref: 'showMarktPointer',
-                        default: true,
-                        options: [{
-                          value: true,
-                          label: 'On'
-                        },{
-                          value: false,
-                          label: 'Off'
-                        }]
-                      },
-                      marktpointShape: {
-                        type: 'string',
-                        component: 'dropdown',
-                        label: 'Marktpoint Shape',
-                        ref: 'marktPointShape',
-                        options: [{
-                          value: 'circle',
-                          label: 'Circle'
-                        },{
-                          value: 'pin',
-                          label: 'Pin'
-                        },{
-                          value: 'arrow',
-                          label: 'Arrow'
-                        },{
-                          value: 'roundRect',
-                          label: 'Rounded Rectangle'
-                        },{
-                          value: 'triangle',
-                          label: 'Triangle'
-                        },{
-                          value: 'rect',
-                          label: 'Rectangle'
-                        },{
-                          value: 'diamond',
-                          label: 'Diamond'
-                        },]
-                      },
-                      marktPointSize: {
-                        type: 'string',
-                        label: 'Marktpoint Size(#)',
-                        ref: 'marktPointSize',
-                        default: '50'
-                      },
-                      marktPointOffSetHeight: {
-                        type: 'string',
-                        label: 'Marktpoint ofset height(#)',
-                        ref: 'marktPointOffSetHeight',
-                        default: '0'
-                      },
-                      marktPointOffSetCenter: {
-                        type: 'string',
-                        label: 'Marktpoint ofset center(#)',
-                        ref: 'marktPointOffSetCenter',
-                        default: '0'
-                      },
-                      marktPointMinData: {
-                        type: 'boolean',
-                        label: 'Show lowest data point',
-                        ref: 'marktPointMinData',
-                        defaultValue: false
-                      },
-                    }
-                  }
-                }
-            },
-
-          },
-        }
-      }
-    },
+    definition: definition,
     initialProperties: {
       qHyperCubeDef: {
         qInitialDataFetch: [
@@ -299,18 +93,6 @@ define(["./echarts-en", "qlik"],function(echarts, qlik){
               var stackAct
             };
 
-            var markPointMinData;
-            if (layout.marktPointMinData == true) {
-              markPointMinData = {type : 'max', name: 'Lowest'}
-            }
-
-            // if (layout.marktPointMinData == true) {
-            //   var markPointMinData = "{type : 'max', name: 'Lowest'},"
-            // } else {
-            //   var markPointMinData
-            // };
-
-
 
             var options = {
             tooltip : {
@@ -347,18 +129,21 @@ define(["./echarts-en", "qlik"],function(echarts, qlik){
                     data: yData,
                     itemStyle: {
                       normal: {
-                        color: layout.meas1.Colour.color
+                        color: layout.qHyperCube.qMeasureInfo[0].Colour.color
                       }
                     },
-                    markPoint : {
-                      symbol: layout.marktPointShape,
-                      symbolSize: layout.marktPointSize,
-                      symbolOffset: [layout.marktPointOffSetCenter, layout.marktPointOffSetHeight],
-                      data: [markPointMinData]
-                    },
+                    markPoint : (layout.qHyperCube.qMeasureInfo[0].showMarktLine===false?{}:{
+                      symbol: layout.qHyperCube.qMeasureInfo[0].marktPointShape,
+                      symbolSize: layout.qHyperCube.qMeasureInfo[0].marktPointSize,
+                      symbolOffset: [layout.qHyperCube.qMeasureInfo[0].marktPointOffSetCenter, layout.qHyperCube.qMeasureInfo[0].marktPointOffSetHeight],
+                      data: [
+                        {type : 'min', name: 'Lowest'},
+                        {type : 'max', name: 'Highest'}
+                      ]
+                    }),
                     markLine : {
                       data : [
-                          {type : 'average'}
+                          {name: 'average', type : 'average'}
                       ]
                     }
                 },
@@ -368,21 +153,21 @@ define(["./echarts-en", "qlik"],function(echarts, qlik){
                     data: cData,
                     itemStyle: {
                       normal: {
-                        color: layout.meas2.Colour.color
+                        color: layout.qHyperCube.qMeasureInfo[1].Colour.color
                       }
                     },
-                    markPoint : {
-                      symbol: layout.marktPointShape,
-                      symbolSize: layout.marktPointSize,
-                      symbolOffset: [layout.marktPointOffSetCenter, layout.marktPointOffSetHeight],
+                    markPoint : (layout.qHyperCube.qMeasureInfo[1].showMarktLine===false?{}:{
+                      symbol: layout.qHyperCube.qMeasureInfo[1].marktPointShape,
+                      symbolSize: layout.qHyperCube.qMeasureInfo[1].marktPointSize,
+                      symbolOffset: [layout.qHyperCube.qMeasureInfo[1].marktPointOffSetCenter, layout.qHyperCube.qMeasureInfo[0].marktPointOffSetHeight],
                       data : [
                         {type : 'min', name: 'Lowest'},
                         {type : 'max', name: 'Highest'}
                       ]
-                    },
+                    }),
                     markLine : {
                       data : [
-                          {type : 'average'}
+                          {name: 'average', type : 'average'}
                       ]
                     }
                 },
